7c73b9d1268c7c288b5665cd5db4affb2bc6b220
krishnapant13 <krishna555.pant@gmail.com>
2024-04-03 17:56:30 +0530

    CORS origin added

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use("/", express.static("uploads"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://your-github-krishnapant13.github.io"],
     credentials: true,
   })
 );
