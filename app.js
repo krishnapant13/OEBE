2db5f93fd468036ff31bfc8390e1789223022e34
krishnapant13 <krishna555.pant@gmail.com>
2024-04-10 11:52:45 +0530

    access-control  added

--- app.js
+++ app.js
@@ -18,6 +18,21 @@ app.use(
     credentials: true,
   })
 );
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://krishnapant13.github.io");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Credentials", true);
+  if (req.method === "OPTIONS") {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
+
 //config
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({
