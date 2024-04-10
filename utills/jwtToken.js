b1ce11c917b929eaba70d0ae9f38fb721a883c3a
krishnapant13 <krishna555.pant@gmail.com>
2024-04-10 12:09:59 +0530

    same-site none added

--- utills/jwtToken.js
+++ utills/jwtToken.js
@@ -5,6 +5,7 @@ const sendToken = (user, statusCode, res) => {
   const options = {
     expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
     httpOnly: true,
+    sameSite: 'None', 
   };
   res.status(statusCode).cookie("token", token, options).json({
     success: true,
